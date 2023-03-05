b43a9944d60b0e89790726f29993247864a83846
Richard <richard_bueno@ymail.com>
2023-03-05 17:54:11 -0300

    feat: adding authentication

--- go-movies-back-end/cmd/api/main.go
+++ go-movies-back-end/cmd/api/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"backend/internal/repository"
+	"backend/internal/repository/dbrepo"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 const port = 3336
 
 type application struct {
-	Domain string
+	DSN          string
+	Domain       string
+	DB           repository.DatabaseRepo
+	auth         Auth
+	JWTSecret    string
+	JWTIssuer    string
+	JWTAudiance  string
+	CookieDomain string
 }
 
 func main() {
@@ -17,13 +28,37 @@ func main() {
 	var app application
 
 	//read from command line
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres coneection string")
+	flag.StringVar(&app.JWTSecret, "jwt-secret", "verysecret", "signing secret")
+	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "example.com", "signing issuer")
+	flag.StringVar(&app.JWTAudiance, "jwt-audiance", "example.com", "signing audiance")
+	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
+	flag.StringVar(&app.Domain, "domain", "example.com", "domain")
+	flag.Parse()
 
 	//connect to the database
-	app.Domain = "example.com"
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+	defer app.DB.Connection().Close()
+
+	app.auth = Auth{
+		Issuer:        app.JWTIssuer,
+		Audience:      app.JWTAudiance,
+		Secret:        app.JWTSecret,
+		TokenExpiry:   time.Minute * 15,
+		RefreshExpiry: time.Hour * 24,
+		CookiePath:    "/",
+		CookieName:    "__Host-refresh_token",
+		CookieDomain:  app.CookieDomain,
+	}
+
 	log.Println("starting application on port", port)
 
 	//start a web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
--- go-movies-back-end/cmd/api/routes.go
+++ go-movies-back-end/cmd/api/routes.go
@@ -13,6 +13,7 @@ func (app *application) routes() http.Handler {
 	mux.Use(middleware.Recoverer)
 	mux.Use(app.enableCORS)
 	mux.Get("/", app.Home)
+	mux.Get("/authenticate", app.authenticate)
 	mux.Get("/movies", app.AllMovies)
 
 	return mux
